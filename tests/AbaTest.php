dc81da61083d438310b88773e4d3d2195b2dfe81
Anam hossain <anam@hotmail.com.au>
2016-07-04 12:42:15 +1000

    tests added for addDetailRecord and addFileTotalRecord

--- tests/AbaTest.php
+++ tests/AbaTest.php
@@ -7,13 +7,60 @@ use Anam\Aba\Aba;
 
 class AbaTest extends TestCase
 {
+    protected $aba;
+
+    public function __construct()
+    {
+        $this->aba = new Aba();
+    }
+
     public function testAddDescriptiveRecord()
     {
-        $expectedString = '0                 01CBA       FOO BAR CORPORATION       301500PAYROLL     290616                                       ';
+        $expectedDescriptiveString = '0                 01CBA       FOO BAR CORPORATION       301500PAYROLL     290616                                        ';
 
-        $aba = new Aba();
+        $descriptveString = $this->aba->addDescriptiveRecord($this->descriptiveData());
 
-        $descriptveString = $aba->addFileDetails([
+        // Total descriptve record would be 120 characters
+        // remove line break
+        $descriptveString = substr($descriptveString, 0, 120);
+
+        $this->assertEquals($expectedDescriptiveString, $descriptveString);
+    }
+
+    public function testAddDetailRecord()
+    {
+        $expectedDetailString = '1111-111999999999 530000025087Jhon doe                            Payroll number062-111111111111FOO BAR         00000000';
+
+        $this->aba->addDescriptiveRecord($this->descriptiveData());
+
+        $detailString = $this->aba->addDetailRecord($this->detailData());
+
+        // Total detail record would be 120 characters
+        // remove line break
+        $detailString = substr($detailString, 0, 120);
+
+        $this->assertEquals($expectedDetailString, $detailString);
+    }
+
+    public function testAddFileTotalRecord()
+    {
+        $expectedFileTotalString = '7999-999            000002508700000250870000000000                        000001                                        ';
+        
+        $this->aba->addDescriptiveRecord($this->descriptiveData());
+        $this->aba->addDetailRecord($this->detailData());
+
+        $filTotalString = $this->aba->addFileTotalRecord();
+
+        // Total detail record would be 120 characters
+        // remove line break
+        $filTotalString = substr($filTotalString, 0, 120);
+
+        $this->assertEquals($expectedFileTotalString, $filTotalString);
+    }
+
+    protected function descriptiveData()
+    {
+        return [
             'bsb' => '062-111', // bsb
             'account_number' => '111111111', // account number
             'bank_name' => 'CBA', // bank name
@@ -22,12 +69,18 @@ class AbaTest extends TestCase
             'user_number' => '301500', // direct entry id for CBA
             'description' => 'PAYROLL', // description
             'process_date'  => '290616' // DDMMYY
-        ]);
+        ];
+    }
 
-        // Total descriptve record would be 120 characters
-        // remove line break
-        $descriptveString = substr($descriptveString, 0, 119);
-
-        $this->assertEquals($expectedString, $descriptveString);
+    protected function detailData()
+    {
+        return [
+            'bsb' => '111-111', // bsb with hyphen
+            'account_number' => '999999999',
+            'account_name'  => 'Jhon doe',
+            'reference' => 'Payroll number',
+            'transaction_code'  => '53',
+            'amount' => '250.87'
+        ];
     }
 }
